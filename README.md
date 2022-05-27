18554946a378c370a07419b4d222a48e7154d792
eliasbokreta <elias.bokreta@gmail.com>
2022-05-27 12:35:07 +0200

    updated dotfiles

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
 # dotfiles
 
-## Dotfiles
-- **bash** : .bash_aliases & .bashrc
-- **git** : .gitconfig
-- **tmux** : .tmux.conf
-- **vim** : .vimrc
-
 ## Script usage
-The `dotfile.sh` script is used to backup / import dotfiles.
-`./dotfile.sh <action> [email]`
-action :
-  - --backup, -b : Backup dotfiles to current directory
-  - --import, -i : Import current directory dotfiles to homedir
-  - [email]      : Used when importing, to replace the hidden email address in the gitconfig file
+The `dotfile.sh` script is used to backup / install dotfiles.
+`./dotfile.sh <action>`
+actions :
+  - --backup, -b  : Backup dotfiles to current directory
+  - --install, -i : Import dotfiles to homedir
--- dotfile.sh
+++ dotfile.sh
@@ -1,29 +1,68 @@
-#         _       _    __ _ _          #
-#        | |     | |  / _(_) |         #
-#      __| | ___ | |_| |_ _| | ___     #
-#     / _` |/ _ \| __|  _| | |/ _ \    #
-#    | (_| | (_) | |_| | | | |  __/    #
-#     \__,_|\___/ \__|_| |_|_|\___|    #
-#                                      #
-# Author : Elias BOKRETA               #
-########################################
-
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
-function usage {
+function usage() {
     echo "Usage : $0 <action> [email]"
     echo "  --backup, -b  : Backup dotfiles to current folder"
-    echo "  --import, -i : Import current file dotfiles to homedir"
-    echo "  [email]       : Replace the hidden email from the gitconfig file"
+    echo "  --install, -i : Import dotfiles to homedir"
 }
 
 if [[ "$#" -eq 0 ]]; then
-    echo "[ERROR] At least one argument is required"
+    echo "[ERROR] You must specify backup or import"
     usage
     exit 1
 fi
+
+function install() {
+    [[ -f zshrc ]] && cp zshrc $HOME/.zshrc
+    [[ -f aliases.zsh ]] && cp aliases.zsh $HOME/.aliases.zsh
+    [[ -f functions.zsh ]] && cp functions.zsh $HOME/.functions.zsh
+
+    [[ -f bashrc ]] && cp bashrc $HOME/.bashrc
+    [[ -f bash_aliases ]] && cp bash_aliases $HOME/.bash_aliases
+
+    mkdir -p $HOME/.config
+    [[ -d config ]] && cp -r config/* $HOME/.config/
+
+    [[ -f gitconfig ]] && cp gitconfig $HOME/.gitconfig
+
+    [[ -f terraformrc ]] && cp terraformrc $HOME/.terraformrc
+    [[ -f tflint.hcl ]] && cp tflint.hcl $HOME/.tflint.hcl
+
+    [[ -f tmux.conf ]] && cp tmux.conf $HOME/.tmux.conf
+    mkdir -p $HOME/.tmux
+    [[ -d tmux ]] && cp -r tmux/* $HOME/.tmux/
+
+    [[ -f tmux ]] && cp vimrc $HOME/.vimrc
+}
+
+function backup() {
+    [[ -f $HOME/.zshrc ]] && cp $HOME/.zshrc zshrc
+    [[ -f $HOME/.aliases.zsh ]] && cp $HOME/.aliases.zsh aliases.zsh
+    [[ -f $HOME/.functions.zsh ]] && cp $HOME/.functions.zsh functions.zsh
+
+    [[ -f $HOME/.bashrc ]] && cp $HOME/.bashrc bashrc
+    [[ -f $HOME/.bash_aliases ]] && cp $HOME/.bash_aliases bash_aliases
+
+    mkdir -p config
+    [[ -d $HOME/.config/bat ]] && cp -r $HOME/.config/bat config/
+
+    [[ -f $HOME/.gitconfig ]] && cp $HOME/.gitconfig gitconfig
+
+    [[ -f $HOME/.terraformrc ]] && cp $HOME/.terraformrc terraformrc
+    [[ -f $HOME/.tflint.hcl ]] && cp $HOME/.tflint.hcl tflint.hcl
+
+    [[ -f $HOME/.tmux.conf ]] && cp $HOME/.tmux.conf tmux.conf
+    [[ -d $HOME/.tmux ]] && cp -r $HOME/.tmux tmux
+
+    [[ -f $HOME/.vimrc ]] && cp $HOME/.vimrc vimrc
+
+    sed -i 's/signingkey =.*/signinkey = <to_replace>/' gitconfig
+    sed -i 's/name =.*/name = <to_replace>/' gitconfig
+    sed -i 's/email =.*/email = <to_replace>/' gitconfig
+}
+
 
 action=$1
 
@@ -33,24 +72,12 @@ case $action in
         exit 0
         ;;
     "--backup" | "-b")
-        cp --verbose ~/.bashrc ./bashrc
-        cp --verbose ~/.bash_aliases ./bash_aliases
-        cp --verbose ~/.gitconfig ./gitconfig
-        cp --verbose ~/.vimrc ./vimrc
-        cp --verbose ~/.tmux.conf ./tmux.conf
+        backup
         echo "Backup done"
-        sed -i -E "s/[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}/<YOUR EMAIL HERE>/g" ./gitconfig
         ;;
-    "--import" | "-i")
-        cp --verbose ./bashrc ~/.bashrc
-        cp --verbose ./bash_aliases ~/.bash_aliases
-        cp --verbose ./gitconfig ~/.gitconfig
-        cp --verbose ./vimrc ~/.vimrc
-        cp --verbose ./tmux.conf ~/.tmux.conf
-        echo "Import done"
-        if [[ "$2" =~ [A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6} ]]; then
-            sed -i -E "s/<YOUR EMAIL HERE>/$2/g" ~/.gitconfig           
-        fi
+    "--install" | "-i")
+        install
+        echo "Dotfiles imported"
         ;;
     *)
         echo "[ERROR] '$1' is not a valid argument"
