cf884306d22d20432c1d9c4dff8c900932914277
eliasbokreta <elias.bokreta@gmail.com>
2023-07-14 19:44:20 +0200

    update dotfiles

--- functions.zsh
+++ functions.zsh
@@ -1,16 +1,27 @@
 #!/bin/zsh
 
+
+#############
+# Git stuff #
+#############
+
 function gdiff() {
+    # Git interactibe diff
     [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = "true" ] || return
-    preview="git diff $@ --color=always -- {-1}"
-    git diff $@ --name-only | fzf -m --ansi --preview $preview --info=inline --border --preview-window 'up,90%,border-bottom,+{2}+3/3,~3'
+    PREVIEW="git diff $@ --color=always -- {-1}"
+    FILE=$(git diff $@ --name-only | fzf -m --ansi --preview $PREVIEW --info=inline --border --preview-window 'up,90%,border-bottom,+{2}+3/3,~3')
+    if [[ $? -eq 0 ]]; then
+        git add $FILE && echo "git added '$FILE'"
+    fi
 }
 
 function gico() {
-   git branch | grep -v "^\*" | fzf --height=20% --reverse --info=inline | xargs git checkout
+    # Git interactive checkout
+    git branch | grep -v "^\*" | fzf --height=20% --reverse --info=inline | xargs git checkout
 }
 
-function tf_remove_empty_ws() {
+function tf_rm_ws() {
+    # Remove empty terraform workspaces
     tws default
     for WS in $(twl | cut -c3-); do
         if [ $WS != "default" ]; then
@@ -20,23 +31,77 @@ function tf_remove_empty_ws() {
     done
 }
 
-function tfmt_all() {
-    # Format all terraform files found. $1 Can be given to add another path than the current one
-    for TF_DIR in $(fd -H -p --type=f "\.(tf?)$" --exclude ".terraform" $1 | sed -E 's|/[^/]+$|/|' | uniq); do
-        echo "[INFO] Formating $TF_DIR"
+
+###################
+# Terraform stuff #
+###################
+
+function tf_fmta() {
+    # Terraform format & lint recursively. $1 optional path
+    DIR=$1
+    for TF_DIR in $(fd -H -p --type=f "\.(tf?)$" --exclude ".terraform" $DIR | sed -E 's|/[^/]+$|/|' | uniq); do
         (
-            cd "$TF_DIR" && \
-            rm -rf .terraform && \
-            t init && \
-            tfmt
+            {
+                echo "[INFO] Formating $TF_DIR" && \
+                cd "$TF_DIR" &&  \
+                rm -rf .terraform && \
+                t init > /dev/null && \
+                tfmt
+            } &
         )
     done
 
-    for TF_VAR in $(fd -H -p --type=f "\.(vars?)$" --exclude ".terraform" . | sed -E 's|/[^/]+$|/|' | uniq); do
-        echo "[INFO] Formating $TF_VAR"
+    for TF_VAR in $(fd -H -p --type=f "\.(vars?)$" --exclude ".terraform" $DIR | sed -E 's|/[^/]+$|/|' | uniq); do
         (
-            cd "$TF_VAR" && \
-            terraform fmt -recursive .
+            {
+                echo "[INFO] Formating $TF_VAR" && \
+                cd "$TF_VAR" && \
+                terraform fmt -recursive .
+            } &
+        )
+    done
+}
+
+function tf_init() {
+    # Terraform init recursively. $1 optional path
+    DIR=$1
+    for TF_DIR in $(fd -H -p --type=f "\.(tf?)$" --exclude ".terraform" $DIR | sed -E 's|/[^/]+$|/|' | uniq); do
+        if [[ -d "$TF_DIR" ]]; then
+            (
+                {
+                    echo "[INFO] Init $TF_DIR" && \
+                    cd "$TF_DIR" && \
+                    rm -rf .terraform && \
+                    rm -rf .terraform.lock.hcl && \
+                    t init > /dev/null && \
+                    tlock
+                } &
+            )
+        else
+            {
+                echo "[INFO] Init current directory" && \
+                rm -rf .terraform && \
+                rm -rf .terraform.lock.hcl && \
+                t init > /dev/null && \
+                tlock
+            } &
+            break
+        fi
+    done
+}
+
+
+####################
+# Kubernetes stuff #
+####################
+
+function k_nv() {
+    # Print all different Kubelet versions in all contexts grouped by labels nodegroup/agentpool (eks/aks)
+    for KCTX in $(kubie ctx | sort); do
+        (
+          kubie ctx $KCTX 2> /dev/null
+          echo $KCTX
+          kubectl get nodes -o jsonpath="{range .items[*]}{.status.nodeInfo.kubeletVersion}{' - '}{.metadata.labels.nodegroup}{.metadata.labels.agentpool}{'\n'}{end}" | sort | uniq -c
         )
     done
 }
